60a76679ea0be0ca200d0833b659822d239578c6
PacoFMD <pacomonfmd@hotmail.com>
2019-09-28 21:49:12 -0500

    Last Commit Successful

--- PrograV_P1S5_VRAPP/Assets/PositionTracking.cs
+++ PrograV_P1S5_VRAPP/Assets/PositionTracking.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GoogleARCore;
 
 public class PositionTracking : MonoBehaviour
 {
+    Vector3 oriPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        oriPos = gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*if(Frame.TrakingState == FrameTracking.Tracking)
+        {
+
+        } */
+
+        gameObject.transform.position = Frame.Pose.position + oriPos;
         
     }
 }
